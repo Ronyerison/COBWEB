6418c482eddafd3f3c5dd6954d363b170cff68ff
Vanderson Moura <vanderson.moura.vm@gmail.com>
2016-02-11 10:14:56 -0300

    Criação da classe responsável pelo cálculo da Utilidade de Categoria de um Nó.

---
+++ cobweb/src/main/java/br/ufpi/easii/cobweb/algorithm/CategoryUtility.java
@@ -0,0 +1,26 @@
+package br.ufpi.easii.cobweb.algorithm;
+
+import br.ufpi.easii.cobweb.model.cobweb.Instance;
+import br.ufpi.easii.cobweb.model.cobweb.NodeInfo;
+import br.ufpi.easii.cobweb.model.tree.Node;
+
+/**
+ * @author Vanderson Moura
+ *
+ */
+public class CategoryUtility { 
+
+	public CategoryUtility() {
+	}
+	
+	public Node<NodeInfo> calculateCategoryUtility(Node<NodeInfo> root, Instance instance){
+		
+		
+		//Calculando as UCs para os nós filhos
+		for (Node<NodeInfo> child : root.getChildren()) {
+			
+		}
+		
+		return null;
+	}
+}
